8d0901718969814f3f8927b460f391e5c524d804
Yutian Wu <wyutian@vmware.com>
2021-03-17 22:02:39 -0700

    cla: add check style to check the Notice Statement before each class

--- src/main/java/com/wavefront/slug/chart/ChartSlugBuilder.java
+++ src/main/java/com/wavefront/slug/chart/ChartSlugBuilder.java
@@ -2,13 +2,14 @@
 Tanzu Observability Slug Generator
 Copyright 2020 VMware, Inc.
 
-This product is licensed to you under the Apache 2.0 license (the "License").  You may not use this
+This product is licensed to you under the Apache 2.0 license (the "License"). You may not use this
 product except in compliance with the Apache 2.0 License.
 
 This product may include a number of subcomponents with separate copyright notices and license terms.
 Your use of these subcomponents is subject to the terms and conditions of the subcomponent's license,
 as noted in the LICENSE file.
 */
+
 package com.wavefront.slug.chart;
 
 import org.joda.time.ReadableInstant;
